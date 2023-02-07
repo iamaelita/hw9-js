a3a859dce0394a761dbebedc71b0025665b2c2f9
Aelita Deinychenko <amm-aelita@ukr.net>
2023-02-07 10:43:25 +0200

    task 1 is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,45 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+//////
+stopBtn.disabled = true;
+let intervalID = null;
+
+
+////
+const randomBodyColorGenerator = {
+  DELAY: 1000,
+
+  getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  },
+
+  interval() {
+    intervalID = setInterval(() => {
+      changeBgColorRandom();
+    }, this.DELAY);
+    stopBtn.disabled = false;
+  },
+
+  start() {
+    startBtn.addEventListener('click', () => {
+      this.interval();
+      startBtn.disabled = true;
+      stopBtn.disabled = false;
+    });
+    stopBtn.addEventListener('click', this.stop);
+  },
+
+  stop() {
+    clearInterval(intervalID);
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+  },
+};
+
+function changeBgColorRandom() {
+  body.style.backgroundColor = `${randomBodyColorGenerator.getRandomHexColor()}`;
+}
+
+randomBodyColorGenerator.start();
